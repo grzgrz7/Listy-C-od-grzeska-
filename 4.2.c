574411363e363bddda3888e9d4e590dfc5bce122
Grzegorz Lipin <288325@student.pwr.edu.pl>
2025-04-22 16:22:18 +0200

    Add files via upload

---
+++ 4.2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Napisz program, który wczytuje ze standardowego wejścia trzy
+// liczby całkowite i wypisuje na standardowym wyjściu ich średnią arytmetyczną
+
+int main()
+{
+
+    int x, y, z;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+
+    float sum = x + y + z;
+    float avg = sum / 3;
+
+    printf("%f\n", avg);
+
+    return 0;
+}
+// done
---
+++ 4.7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+
+    float x, y;
+    scanf("%f", &x);
+    scanf("%f", &y);
+
+    float quotient = x / y;
+
+    printf("%f\n", quotient);
+
+    return 0;
+}
+
+// done
